47bcd8a63ae791049f3a88fabda171cbb0e10063
dilan2 <serglebko@gmail.com>
2016-12-20 22:35:07 +0300

    now only 1 query

--- hm8/01/indexOLD.js
+++ hm8/01/indexOLD.js
@@ -8,13 +8,20 @@ function func() {
     }).then(function () {
         getCities('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json').then(function (response) {
             var cont = JSON.parse(response);
+
             function sorter(a, b) {
                 return a.name.localeCompare(b.name);
             }
 
             cont.sort(sorter);
-            temp = cont;
-            var contres = cont;
+            var data = input.value;
+            if (!data) {
+                var contres = cont;
+            } else {
+                var contres = cont.filter(function (arg) {
+                    return arg.name.indexOf(data) > -1;
+                })
+            }
             let source = citiesList.innerHTML;
             let templFn = Handlebars.compile(source);
             let template = templFn({items: contres});
@@ -38,26 +45,8 @@ function getCities(url) {
 }
 func();
 var input = document.getElementsByTagName('INPUT')[0];
-input.addEventListener('input', func2);
+input.addEventListener('input', func);
 
-
-var temp = [];
-
-
-function func2() {
-    var data = input.value;
-    if (!data) {
-        var contres = temp;
-    } else {
-        var contres = temp.filter(function (arg) {
-            return arg.name.indexOf(data) > -1;
-        })
-    }
-    let source = citiesList.innerHTML;
-    let templFn = Handlebars.compile(source);
-    let template = templFn({items: contres});
-    list.innerHTML = template;
-}
 
 
 
